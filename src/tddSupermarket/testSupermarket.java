f8e0ed8cff6b14102d268f478a7c8ec228f30436
masayuki tanaka <masayuki5160@gmail.com>
2018-01-02 22:28:11 +0900

    import文をリファクタリング

--- src/tddSupermarket/testSupermarket.java
+++ src/tddSupermarket/testSupermarket.java
@@ -5,17 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
-
-import tddSupermarket.item.Apple;
-import tddSupermarket.item.Coffee;
-import tddSupermarket.item.Grape;
-import tddSupermarket.item.Lighter;
-import tddSupermarket.item.Mikan;
-import tddSupermarket.item.Noriben;
-import tddSupermarket.item.Shakeben;
-import tddSupermarket.item.Tea;
-import tddSupermarket.item.Tobacco;
-import tddSupermarket.item.TobaccoMenthol;
+import tddSupermarket.item.*;
 
 @RunWith(Enclosed.class)
 public class testSupermarket {
