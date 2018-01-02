22e2ea76db4bc9115f269f9c7f1227fa7ef18787
masayuki tanaka <masayuki5160@gmail.com>
2018-01-02 21:41:16 +0900

    事前処理を追加

--- src/tddSupermarket/testSupermarket.java
+++ src/tddSupermarket/testSupermarket.java
@@ -1,6 +1,8 @@
 package tddSupermarket;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
@@ -10,12 +12,18 @@ public class testSupermarket {
 
 	public static class 商品番号と個数から合計金額を計算する{
 		
+		private Cart cart;
+		
+		@Before
+		public void 事前処理() {
+			cart = new Cart();
+		}
+		
 		@Test
 		public void りんご1個みかん2個から合計金額を計算する() {
 			Apple apple = new Apple(1);
 			Mikan mikan = new Mikan(2);
 			
-			Cart cart = new Cart();
 			cart.add(apple);
 			cart.add(mikan);
 			
@@ -28,7 +36,6 @@ public class testSupermarket {
 		public void りんご1個から合計金額を計算する() {
 			Apple apple = new Apple(1);
 			
-			Cart cart = new Cart();
 			cart.add(apple);
 			
 			int sum = cart.checkout();
