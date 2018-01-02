6780af81f781506244982588b05a4eaa1dac9aee
masayuki tanaka <masayuki5160@gmail.com>
2018-01-02 21:22:48 +0900

    enclosed classを使ってテストクラスを構造化する

--- src/tddSupermarket/testSupermarket.java
+++ src/tddSupermarket/testSupermarket.java
@@ -2,21 +2,26 @@ package tddSupermarket;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
 
+@RunWith(Enclosed.class)
 public class testSupermarket {
 
-	@Test
-	public void 商品番号と個数から合計金額を計算する() {
-		Apple apple = new Apple(1);
-		Mikan mikan = new Mikan(2);
-		
-		Cart cart = new Cart();
-		cart.add(apple);
-		cart.add(mikan);
-		
-		int sum = cart.checkout();
-		
-		assertEquals(100 + 40 * 2, sum);
+	public static class 商品番号と個数から合計金額を計算する{
+		@Test
+		public void りんご1個みかん2個から合計金額を計算する() {
+			Apple apple = new Apple(1);
+			Mikan mikan = new Mikan(2);
+			
+			Cart cart = new Cart();
+			cart.add(apple);
+			cart.add(mikan);
+			
+			int sum = cart.checkout();
+			
+			assertEquals(100 + 40 * 2, sum);
+		}
 	}
 
 }
