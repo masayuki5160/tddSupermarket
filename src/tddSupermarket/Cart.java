c61e2b01dfa34ccdc64c52a450eda0d23df33f69
masayuki tanaka <masayuki5160@gmail.com>
2018-01-02 21:18:10 +0900

    購入した商品の計算処理をリファクタリング

--- src/tddSupermarket/Cart.java
+++ src/tddSupermarket/Cart.java
@@ -1,14 +1,23 @@
 package tddSupermarket;
 
-import tddSupermarket.Item;
+import java.util.*;
 
 public class Cart {
 
+	private Collection<Item> items = new ArrayList<Item>();
+
 	public int checkout() {
-		return 180;
+		int sum = 0;
+		Iterator<Item> iterator = items.iterator();
+		while(iterator.hasNext()) {
+			Item item = iterator.next();
+			sum += item.getPrice() * item.getCount();// TODO Iteratorを使ってる時に他の方法で合計計算できないの？
+		}
+		return sum;
 	}
 
 	public void add(Item item) {
+		items.add(item);
 	}
 
 }
